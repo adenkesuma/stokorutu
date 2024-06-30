699ed6b3a3902cf771c631c05c33e9c700c3d04a
adenkesuma <adenfdfd10@gmail.com>
2024-06-30 09:37:59 +0700

    `add video & notebook`

--- components/footer.tsx
+++ components/footer.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <footer className='mx-auto py-8 w-full border-t flex justify-center'>
+    <footer className='mx-auto mt-20 py-8 w-full border-t flex justify-center'>
         <p>
             &copy; 2024 Copyright Stokorutu. All rights reserved. 
         </p>
